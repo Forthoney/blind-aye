57222b9612ff12b2a76d21bff86afaf65ccb098e
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-06-11 00:32:55 +0900

    create main menu button script with partial implementation

---
+++ BlindAye/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
